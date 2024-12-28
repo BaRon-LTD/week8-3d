77a7d2691618460c31afd3afbd835795dadfdbd0
ron_shuster <118674717+shusteron@users.noreply.github.com>
2024-12-28 21:25:36 +0200

    movement working

--- Assets/Scripts/Player/CharacterKeyboardMover.cs
+++ Assets/Scripts/Player/CharacterKeyboardMover.cs
@@ -7,17 +7,19 @@ using UnityEngine.InputSystem;
  * This component moves a player controlled with a CharacterController using the keyboard.
  */
 [RequireComponent(typeof(CharacterController))]
-public class CharacterKeyboardMover: MonoBehaviour {
+public class CharacterKeyboardMover : MonoBehaviour
+{
     [Tooltip("Speed of player keyboard-movement, in meters/second")]
     [SerializeField] float speed = 3.5f;
     [SerializeField] float gravity = 9.81f;
 
     private CharacterController cc;
-    
+
     [SerializeField] InputAction moveAction;
     private void OnEnable() { moveAction.Enable(); }
     private void OnDisable() { moveAction.Disable(); }
-    void OnValidate() {
+    void OnValidate()
+    {
         // Provide default bindings for the input actions.
         // Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
         if (moveAction == null)
@@ -31,24 +33,42 @@ public class CharacterKeyboardMover: MonoBehaviour {
                 .With("Right", "<Keyboard>/d");
     }
 
-    void Start() {
+    void Start()
+    {
         cc = GetComponent<CharacterController>();
     }
 
-    Vector3 velocity = new Vector3(0,0,0);
+    Vector3 velocity = new Vector3(0, 0, 0);
 
-    void Update()  {
-        if (cc.isGrounded) {
-            Vector3 movement = moveAction.ReadValue<Vector2>(); // Implicitly convert Vector2 to Vector3, setting z=0.
+    void Update()
+    {
+        // Read movement input as Vector2
+        Vector2 input = moveAction.ReadValue<Vector2>();
+
+        // Convert input to a Vector3 for movement on the XZ plane
+        Vector3 movement = new Vector3(input.x, 0, input.y);
+
+        // Transform the movement vector to match the player's orientation
+        movement = transform.TransformDirection(movement);
+
+        if (cc.isGrounded)
+        {
+            // Apply movement input to the velocity
             velocity.x = movement.x * speed;
-            velocity.z = movement.y * speed;
-        } else {
-            velocity.y -= gravity*Time.deltaTime;
+            velocity.z = movement.z * speed;
+
+            // Reset vertical velocity when grounded
+            velocity.y = 0;
+        }
+        else
+        {
+            // Apply gravity if the player is not grounded
+            velocity.y -= gravity * Time.deltaTime;
         }
 
-        // Move in the direction you look:
-        velocity = transform.TransformDirection(velocity);
-
+        // Apply movement through the CharacterController
         cc.Move(velocity * Time.deltaTime);
     }
+
+
 }
--- Assets/Scripts/Player/InputRotator.cs
+++ Assets/Scripts/Player/InputRotator.cs
@@ -1,46 +1,55 @@
 ﻿﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 /**
- * This component rotates its object according to the mouse movement, in a given rotation speed.
- */ 
+ * This component rotates the player horizontally and the camera vertically based on mouse movement.
+ */
 public class InputRotator : MonoBehaviour {
     [SerializeField] float rotationSpeed = 0.1f;
 
-    [Tooltip("Rotate the object according to the mouse X-axis movement?")]
+    [Tooltip("Rotate the player object horizontally with the mouse X-axis movement?")]
     [SerializeField] bool horizontalRotation = true;
-    [SerializeField] float minHorizontalRotation = -360f;
-    [SerializeField] float maxHorizontalRotation = 360f;
 
-    [Tooltip("Rotate the object according to the mouse Y-axis movement?")]
+    [Tooltip("Rotate the camera object vertically with the mouse Y-axis movement?")]
     [SerializeField] bool verticalRotation = true;
+
     [SerializeField] float minVerticalRotation = -45f;
     [SerializeField] float maxVerticalRotation = 45f;
 
     [SerializeField] InputAction lookLocation = new InputAction(type: InputActionType.Value);
-    void OnEnable() {        lookLocation.Enable();    }
-    void OnDisable() {        lookLocation.Disable();   }
+
+    [SerializeField] Transform playerBody; // Reference to the Player GameObject
+    [SerializeField] Transform cameraTransform; // Reference to the Camera GameObject
+
+    void OnEnable() { lookLocation.Enable(); }
+    void OnDisable() { lookLocation.Disable(); }
+
     void OnValidate() {
-        // Provide default bindings for the input actions. https://gamedev.stackexchange.com/a/205345/18261
-        if (lookLocation.bindings.Count == 0)
+        if (lookLocation.bindings.Count == 0) {
             lookLocation.AddBinding("<Mouse>/delta");
+        }
     }
 
     void Update() {
         Vector2 mouseDelta = lookLocation.ReadValue<Vector2>();
-        Vector3 rotation = transform.localEulerAngles;
 
-        if (horizontalRotation) {
-            rotation.y = Mathf.Clamp(rotation.y + mouseDelta.x * rotationSpeed, minHorizontalRotation, maxHorizontalRotation);  // Rotation around the vertical (Y) axis
-        }
-        if (verticalRotation) {
-            float newRotationX = rotation.x - mouseDelta.y * rotationSpeed;
-            if (newRotationX > 180f)
-                newRotationX -= 360f;
-            float ClampedRotationX = Mathf.Clamp(newRotationX, minVerticalRotation, maxVerticalRotation);
-            rotation.x = ClampedRotationX;
+        // Horizontal rotation (applied to the Player object)
+        if (horizontalRotation && playerBody != null) {
+            float horizontalRotationAmount = mouseDelta.x * rotationSpeed;
+            playerBody.Rotate(Vector3.up, horizontalRotationAmount, Space.World); // Rotate around global Y-axis
         }
 
-        transform.localEulerAngles = rotation;
+        // Vertical rotation (applied to the Camera only)
+        if (verticalRotation && cameraTransform != null) {
+            float verticalRotationAmount = -mouseDelta.y * rotationSpeed; // Invert for correct direction
+            float currentRotationX = cameraTransform.localEulerAngles.x;
+
+            if (currentRotationX > 180f)
+                currentRotationX -= 360f;
+
+            float newRotationX = Mathf.Clamp(currentRotationX + verticalRotationAmount, minVerticalRotation, maxVerticalRotation);
+            cameraTransform.localEulerAngles = new Vector3(newRotationX, 0, 0);
+        }
     }
 }
